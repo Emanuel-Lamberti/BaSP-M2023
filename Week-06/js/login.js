3f8a2549a916f5b0e15dd5b5bd3286398854d0e7
Emanuel-Lamberti <elemanu9222@gmail.com>
2023-04-22 19:03:25 -0300

    BaSP-W06-2023: modified login.js file

--- Week-06/js/login.js
+++ Week-06/js/login.js
@@ -1,21 +1,36 @@
 var emailInput = document.getElementById("Email");
+var textValidationEmail = document.getElementById("textValidationEmail");
+var textValidationPassword = document.getElementById("textValidationPassword");
+
 emailInput.onfocus = function() {
     emailInput.classList.add("green-border");
+    emailInput.classList.remove("red-border");    
 }
 
 emailInput.onblur = function() {
-    emailInput.classList.remove("green-border");
+    let emailValido = validationEmail();
+    if(emailValido == true){
+        emailInput.classList.remove("green-border");
+        textValidationEmail.style.display = "none";
+    }else{
+        emailInput.classList.add("red-border");
+        textValidationEmail.style.display = "block";
+    }
 }
 
 var validEmail =  /^\w+([.-_+]?\w+)*@\w+([.-]?\w+)*(\.\w{2,10})+$/;
-var email = document.getElementById("Email").value;
-var listEmail = email.split(".");
-function validationEmail(email) {
-    if (validEmail.test(email) && listEmail.pop().length < 4 && email != null && email.length > 0) {
-        alert("Correct email");
+var email;
+var listEmail;
+function validationEmail() {
+    email = document.getElementById("Email").value;
+    listEmail = email.split(".");
+    if (
+    validEmail.test(email) && 
+    listEmail.pop().length >= 3 &&    
+    email != null &&
+    email.length > 0) {
         return true;
     } else {
-        alert("Email invalid");
         return false;
     }
 }
@@ -23,27 +38,46 @@ function validationEmail(email) {
 var passwordInput = document.getElementById("Password");
 passwordInput.onfocus = function() {
     passwordInput.classList.add("green-border");
+    passwordInput.classList.remove("red-border");
 }
 
 passwordInput.onblur = function() {
-    passwordInput.classList.remove("green-border");
+    
+    let passValido = validationPassword();
+    if(passValido == true){
+        passwordInput.classList.remove("green-border");
+        textValidationPassword.style.display = "none";
+    }else{
+        passwordInput.classList.add("red-border");
+        textValidationPassword.style.display = "block";
+    }
 }
 
-var password = document.getElementById("Password").value;
-function validationPassword(password) {
-    if (password.length >= 8 && password != null && password.length > 0 && isNaN(password) == true) {
-        alert("Correct password");
+var password;
+function validationPassword() {
+    password = document.getElementById("Password").value
+    if (password.length >= 8 && password != null && isNaN(password) == true) {
         return true;
     } else {
-        alert("Incorrect password");
         return false;
     }
 }
 
 var acceptBtn = document.getElementById("enter");
 acceptBtn.onclick = function(event) {
+    var emailValido = validationEmail();
+    var passwordValido = validationPassword();
+    if(emailValido && passwordValido){ 
+        alert("Email: " + emailInput.value + "\nPassword: " + passwordInput.value);
+    } else {
+        if(emailValido == false){
+            alert("Invalid email: " + emailInput.value);
+        }
+        if(passwordValido == false) {
+            alert("Invalid password: " + passwordInput.value);
+        }
+    }
     event.preventDefault();
     console.log(emailInput.value);
     console.log(passwordInput.value);
-    alert("Email: " + emailInput.value + "\nPassword: " + passwordInput.value);
 }
